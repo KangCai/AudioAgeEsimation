33a023f41041243f3f227a47fc8d8e3e53a52d5b
Kang Cai <caikangpku@gmail.com>
2016-03-22 10:50:16 +0800

    2016-03-22-10

--- src/com/example/utils/BasicFeatureExtraction.java
+++ src/com/example/utils/BasicFeatureExtraction.java
@@ -3,11 +3,6 @@ package com.example.utils;
 import java.io.File;
 import java.io.IOException;
 
-import javax.sound.sampled.AudioFormat;
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.UnsupportedAudioFileException;
-
 
 
 public class BasicFeatureExtraction {
